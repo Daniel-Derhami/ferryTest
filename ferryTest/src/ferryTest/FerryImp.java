8cc46a5ac2a4b594a3a53ca9ce02b4134de07a81
DanDer <daniel.derhami@gmail.com>
2017-02-13 00:03:50 +0100

    Ferry Implmenetation

---
+++ ferryTest/src/ferryTest/FerryImp.java
@@ -0,0 +1,53 @@
+package ferryTest;
+
+import java.util.Iterator;
+
+
+public class FerryImp implements FerryAB {
+
+	public int countPassengers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int countVehicleSpace() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int countMoney() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Iterator<Vehicles> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void embark(Vehicles ve) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void embark(Passengers pas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void disembark() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean hasSpaceFor(Vehicles ve) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean hasRoomForPassenger() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
