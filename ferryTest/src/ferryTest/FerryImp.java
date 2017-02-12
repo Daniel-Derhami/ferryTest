06cb31f2ac3f844252beadeac9a1251953d270d0
DanDer <daniel.derhami@gmail.com>
2017-02-13 00:15:36 +0100

    Ferry Implementation

--- ferryTest/src/ferryTest/FerryImp.java
+++ ferryTest/src/ferryTest/FerryImp.java
@@ -10,6 +10,7 @@ public class FerryImp implements FerryAB {
 	private int TotalVehicles = 0;
 	private int TotalMoney = 0;
 	private int TotalPassengers = 0;
+	private final int VEHSPACE = 40, PASSSPACE = 200;
 	private ArrayList<Vehicles> Vehclelist = new ArrayList<Vehicles>();
 	
 	
@@ -31,8 +32,23 @@ public class FerryImp implements FerryAB {
 	}
 
 	public void embark(Vehicles ve) {
-		// TODO Auto-generated method stub
-		
+		int n = ve.getPassNumbers();
+		if (n <= (PASSSPACE - TotalPassengers) && hasSpaceFor(ve)
+				&& n <= ve.getMaxPassenger()) {
+			for (int i = 1; i <= n; i++) {
+				TotalPassengers++;
+				TotalMoney += ve.getPassPrice();
+
+			}
+			TotalMoney += ve.getPrice();
+			Vehclelist.add(ve);
+			TotalVehicles += ve.getCapacity();
+			System.out
+					.print("*Welcome! Your Vehicel and the passengers Embarked succesfully");
+		} else
+			System.out
+					.print("OOPS! : This can not be embarked by these conditions! Sorry. Chek your inputs and try again");
+
 	}
 
 	public void embark(Passengers pas) {
@@ -46,13 +62,17 @@ public class FerryImp implements FerryAB {
 	}
 
 	public boolean hasSpaceFor(Vehicles ve) {
-		// TODO Auto-generated method stub
-		return false;
+		if (VEHSPACE - TotalVehicles > ve.getCapacity())
+			return true;
+		else
+			return false;
 	}
 
 	public boolean hasRoomForPassenger() {
-		// TODO Auto-generated method stub
-		return false;
+		if (PASSSPACE > TotalPassengers)
+			return true;
+		else
+			return false;
 	}
 
 }
